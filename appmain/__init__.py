fa6af23a7a81c1a03ff0be51de0b4f31a042b631
davidmh0203 <davidmh0203@gmail.com>
2025-06-05 11:06:03 +0900

    기본 홈 UI, auth 구현

---
+++ appmain/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_mail import Mail
+
+# Flask 앱 객체 생성
+app = Flask(__name__)
+
+# 기본 설정
+app.config["SECRET_KEY"] = 'e2a14e9612b8bdfc57201cfce12b6c8f'
+
+# 메일 서버 설정
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = 'YOUR_ACCOUNT'    # 실제 Gmail 계정
+app.config["MAIL_PASSWORD"] = 'YOUR_PASSWORD'    # 앱 비밀번호 (2단계 인증 필요 시)
+
+# Mail 객체 생성
+mail = Mail(app)
+
+# 블루프린트 등록
+from appmain.routes import main
+app.register_blueprint(main)
+
+from appmain.user.routes import user
+app.register_blueprint(user)
---
+++ appmain/utils.py
@@ -0,0 +1,52 @@
+import os.path
+import secrets
+from PIL import Image
+
+import jwt
+import sqlite3
+
+from appmain import app
+
+def verifyJWT(token):
+    if token is None:
+        return None
+    else:
+        try:
+            decodedToken = jwt.decode(token, app.config["SECRET_KEY"], algorithms="HS256")
+            if decodedToken:
+                conn = sqlite3.connect('pyBook.db')
+                cursor = conn.cursor()
+
+                if cursor:
+                    SQL = 'SELECT authkey FROM users WHERE email=?'
+                    cursor.execute(SQL, (decodedToken["email"],))
+                    authkey = cursor.fetchone()[0]
+                    cursor.close()
+                if authkey == decodedToken["authkey"]:
+                    return True
+                else:
+                    return None
+            else:
+                return None
+        except:
+            return  None
+
+def getJWTContent(token):
+    isVerified = verifyJWT(token)
+
+    if isVerified:
+        return jwt.decode(token, app.config["SECRET_KEY"], algorithms="HS256")
+    else:
+        return None
+
+def savePic(pic, username):
+    randHEX =secrets.token_hex(8)
+    _, fExt = os.path.splitext(pic.filename)
+    picFileName = randHEX + fExt
+    picDir = os.path.join(app.static_folder, 'pics', username)
+    picPath = os.path.join(picDir, picFileName)
+    os.makedirs(picDir, exist_ok=True)
+
+    with Image.open(pic) as image:
+        image.save(picPath)
+    return picFileName
