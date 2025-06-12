df77d0c774fec330f7f3c3eb71b56d43b19ffdbc
davidmh0203 <davidmh0203@gmail.com>
2025-06-12 10:19:22 +0900

    배포에 맞게 설정 변경

--- appmain/db.py
+++ appmain/db.py
@@ -1,16 +1,16 @@
+import os
 import pymysql
 from dbutils.pooled_db import PooledDB
-from pymysql import cursors
-
 
 POOL = PooledDB(
     creator=pymysql,
     maxconnections=5,
     blocking=True,
-    host='localhost',
-    user='root',
-    password='12341234',
-    database='bus_eta',
+    host=os.environ.get('DB_HOST'),
+    port=int(os.environ.get('DB_PORT', 3306)),
+    user=os.environ.get('DB_USER'),
+    password=os.environ.get('DB_PASSWORD'),
+    database=os.environ.get('DB_NAME'),
     cursorclass=pymysql.cursors.DictCursor
 )
 
